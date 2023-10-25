23e9348f4787f6b7e42fbc3899dbe8d378052c64
Nathan <nathanlucaass.silva@gmail.com>
2023-10-25 18:17:26 -0300

    css la vai mais airbnb...

--- app/frontend/src/pages/LoginPage.jsx
+++ app/frontend/src/pages/LoginPage.jsx
@@ -1,15 +1,15 @@
 import React, { useContext, useState } from 'react';
-import { useHistory } from 'react-router-dom'; 
+import { useHistory } from 'react-router-dom';
 import callLoginApi from '../services/loginAPI';
 import UserInfoContext from '../context/UserInfoContext';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState(''); 
-  const history = useHistory(); 
+  const [error, setError] = useState('');
+  const history = useHistory();
 
-  const {setUserId, setUserToken} = useContext(UserInfoContext);
+  const { setUserId, setUserToken } = useContext(UserInfoContext);
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -35,22 +35,31 @@ function Login() {
   };
 
   return (
-    <div className='bg-red-500'>
-    <form onSubmit={handleSubmit}>
-      <label>
-        Email:
-        <input type="email" value={email} onChange={handleEmailChange} />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input type="password" value={password} onChange={handlePasswordChange} />
-      </label>
-      <br />
-      <button type="submit">Login</button>
-      <h3>{error}</h3>
-      <a href="/register">Ainda não possuo cadastro</a>
-    </form>
+    <div className="flex justify-center items-center h-screen">
+      <div className="bg-white p-4 max-w-md mx-auto border rounded shadow">
+        <form onSubmit={handleSubmit}>
+          <div className="text-center mb-4">
+            <img src="https://i.ibb.co/0FVRL61/Financial.png" width={100} className="mx-auto" alt="Logo" />
+          </div>
+          <label>
+            Email:
+            <input className="block w-full mt-2 p-2 border rounded" type="email" value={email} onChange={handleEmailChange} />
+          </label>
+          <br />
+          <label>
+            Password:
+            <input className="block w-full mt-2 p-2 border rounded" type="password" value={password} onChange={handlePasswordChange} />
+          </label>
+          <br />
+          <button className="block w-full bg-blue-500 text-white mt-4 p-2 rounded" type="submit">
+            Login
+          </button>
+          <h3 className="text-red-500 mt-2">{error}</h3>
+          <a className="mt-2 block text-blue-500" href="/register">
+            Ainda não possuo cadastro
+          </a>
+        </form>
+      </div>
     </div>
   );
 }
